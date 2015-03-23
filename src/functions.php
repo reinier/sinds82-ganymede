3dcddd4491fd80881eae07b18e30066bf8d44d67
Reinier Ladan <reinierl@gmail.com>
2015-03-23 15:34:40 +0100

    No navigation in this theme

--- src/functions.php
+++ src/functions.php
@@ -1,12 +1,11 @@
 <?php
 
-  add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-  function theme_enqueue_styles() {
+  add_action( 'wp_enqueue_scripts', 'rl_theme_enqueue_styles' );
+  function rl_theme_enqueue_styles() {
 
     // wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
       get_stylesheet_directory_uri() . '/style.css'
     );
   }
-
 ?>
